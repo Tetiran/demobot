c3e18ff560ac2e5ac1fa37a5978289f29d588a03
Tetiran <justinpaul616@gmail.com>
2018-01-20 11:32:57 -0500

    Initial commit/ stuff happened

---
+++ Demo Bot/src/org/usfirst/frc/team1277/robot/commands/Drive.java
@@ -0,0 +1,5 @@
+package org.usfirst.frc.team1277.robot.commands;
+
+public class Drive {
+
+}
---
+++ Demo Bot/src/org/usfirst/frc/team1277/robot/subsystems/Drivetrain.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc.team1277.robot.subsystems;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Drivetrain extends Subsystem{
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
